8acb9401511717999097be3b0d41b4e24670cd8f
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-01-01 14:32:15 +0300

    Dont forget to add your api key to Constants.kt file

--- app/src/main/java/com/cleanapp/util/Constants.kt
+++ app/src/main/java/com/cleanapp/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.cleanapp.util
 
 object Constants {
+    TODO("Add your Api key")
     const val API_KEY = "add your apikey here"
     const val BASE_URL = "https://api.collectapi.com/"
     const val CONTENT_TYPE = "application/json"
